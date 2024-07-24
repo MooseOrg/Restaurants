f02ef2afc49d83f1bf35c3e154d46d25f87b32fc
Kim Reneil Paule <kim.paule@mvp.net.ph>
2024-07-24 19:17:48 +0800

    Add project files.

---
+++ Restaurants.API/Controllers/IWeatherForecastService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Restaurants.API.Controllers
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> Get(int count, int minTemperature, int maxTemperature);
+    }
+}
