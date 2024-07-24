ee5f473762e5d25f6a39bddc6049c57ab89d178c
Kim Reneil Paule <kim.paule@mvp.net.ph>
2024-07-24 20:53:59 +0800

    Added controller, service and repository for restaurant

---
+++ Restaurants.API/Controllers/RestaurantController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Restaurants;
+
+
+namespace Restaurants.API.Controllers;
+
+[ApiController]
+[Route("api/restaurants")]
+public class RestaurantController(IRestaurantsService restaurantsService) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var restaurants = await restaurantsService.GetAllRestaurants();
+
+        return Ok(restaurants);
+    }
+}
